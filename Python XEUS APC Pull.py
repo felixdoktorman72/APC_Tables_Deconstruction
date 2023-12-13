340e2654a1865af668f60bf43790bc3f5c969580
felixdoktorman72 <148132239+felixdoktorman72@users.noreply.github.com>
2023-12-13 12:09:39 +0200

    Empty Chambers Bug fix and Parquet saving option

--- Python XEUS APC Pull.py
+++ Python XEUS APC Pull.py
@@ -71,7 +71,7 @@ def DataExtractFromXEUS():
                                                         ,'LAMBDA_PART_USED','PM_COUNTER_PRIOR','PM_COUNTER','REFERENCE_SETTING','M_ETCHRATE','METRO_LOLIMIT','METRO_HILIMIT'
                                                         ,'BATCH_ID','RSTIME','SHORTWAFERIDS','CHAMBER','CHAMBER_IDX','VALIDDATA','APC_DATA_ID','UPTIME','METROAVG_CHBR'
                                                         ,'MACHINE', 'MOMLOT', 'SMTIME', 'LAMBDA_TOOL','LAMBDA_PART') 
-        and ah.LOAD_DATE >= SYSDATE - 7
+        and ah.LOAD_DATE >= SYSDATE - 60
     '''
         lotcursor = conn.execute(myQuery)
         field_name = [field[0] for field in lotcursor.description]      
@@ -105,12 +105,13 @@ def DataQualityChecks(df_pivot):
     # #removing rows with empty batch id and chambers
     df_pivot_checked = df_pivot
     df_pivot_checked.dropna(subset = ['BATCH_ID'], inplace = True)
-    df_pivot_checked.drop(df_pivot[df_pivot.CHAMBER == '[NULL]'].index, inplace = True)
     
+    # Remove rows with [NULL] or None in the 'CHAMBER' column
+    df_pivot_checked = df_pivot_checked[df_pivot_checked['CHAMBER'].notna() & (df_pivot_checked['CHAMBER'] != '[NULL]')]
       
     #key definition for parsing
     df_pivot_checked['KEY'] = df_pivot_checked['BATCH_ID'] + "_" + df_pivot_checked['SUBENTITY']
-    df_pivot.fillna('[NULL]', inplace=True)
+    #df_pivot.fillna('[NULL]', inplace=True)
     
     #Need to check if any UPTIME empty cells
     
@@ -132,8 +133,20 @@ def WaferChamberAssociation(df):
     mes_slots_wafers = sorted(zip(MES_SLOTS_LIST,MES_WAFER_IDS_LIST, MES_WID3), key = lambda x:int(x[0]))
     mes_slot_wafers_df = pd.DataFrame(list(mes_slots_wafers), columns = ['MES_SLOT', 'WID', 'WID3'])
     
-    wafers_by_chamber = sorted(zip(CHAMBER_LIST, CHAMBER_IDX_LIST, SHORTWAFERID_LIST, SLOTS_LIST),key = lambda x:int(x[1])) 
-    wafers_chambers_df = pd.DataFrame(list(wafers_by_chamber), columns = ['Chamber', 'Slot', 'WID3', 'MES_SLOT'])
+    try: 
+        wafers_by_chamber = sorted(zip(CHAMBER_LIST, CHAMBER_IDX_LIST, SHORTWAFERID_LIST, SLOTS_LIST),key = lambda x:int(x[1])) 
+    except ValueError:
+        print(f" BATCH_ID is {df['BATCH_ID']}")
+        print(f" CHAMBER_LIST is {CHAMBER_LIST}")
+        print(f" CHAMBER_IDX_LIST is {CHAMBER_IDX_LIST}")
+        print(f" SHORTWAFERID_LIST is {SHORTWAFERID_LIST}")
+        print(f" SLOTS_LIST is {SLOTS_LIST}")
+        
+    try:
+        wafers_chambers_df = pd.DataFrame(list(wafers_by_chamber), columns = ['Chamber', 'Slot', 'WID3', 'MES_SLOT'])
+    except UnboundLocalError:
+        print("UnboundLocalError")
+        print(f"wafers_by_chamber {wafers_by_chamber}")
     
     wafer_df = pd.merge(mes_slot_wafers_df, wafers_chambers_df, on=["MES_SLOT","WID3"])
     subentity3_wafer_df = wafer_df[wafer_df.Chamber == SUBENTITY3]
@@ -196,98 +209,71 @@ def FB_Metro_by_Slot(df_fb_data,df_fb_idx,WLV_slot):
 
 def WaferLevelData(df):
     
-    #APC_UI_ORDER = []
-    LVL_Columns=['AREA', 'BATCH_ID', 'B_PART', 'B_PART_PRIOR', 'B_PART_RS', 'B_TOOL',
-           'B_TOOL_PRIOR', 'B_TOOL_RS', 'CALCULATED_SETTING', 'FB_SUC',
+    
+    LVL_Columns=['LOAD_DATE','FACILITY','AREA', 'BATCH_ID', 'B_PART', 
+           'B_TOOL_PRIOR', 'CALCULATED_SETTING', 'FB_SUC',
            'FB_TARGET', 'FF_SUC', 'LAMBDA_PART', 'LAMBDA_PART_USED', 'LAMBDA_TOOL',
            'LAMBDA_TOOL_USED', 'LOTID', 'LOTSETTINGS',  'METROAVGLOT', 'METROAVG_CHBR',
            'METRO_HILIMIT', 'METRO_LOLIMIT', 'MOMLOT', 'M_ETCHRATE', 'OPENRUNS',
            'OPENRUNS_PART', 'PM_COUNTER', 'PM_COUNTER_PRIOR', 'PROCESS',
            'PROCESS_OPN', 'PRODGROUP', 'PRODUCT', 'REFERENCE_SETTING', 'ROUTE',
-           'RSTIME', 'SETTING_USED', 'SMTIME','SUBENTITIES', 'SUBENTITY', 'TARGET', 'UPDATE_TIME', 'UPTIME',
+           'RSTIME',  'SMTIME','SUBENTITIES', 'SUBENTITY', 'UPDATE_TIME', 'UPTIME',
            'VALIDDATA', 'WAFERSETTINGS',  'KEY']
             
     WLV = {}
-    WLV['LOT'] = df['LOTID']
-    WLV['KEY'] = df['KEY']
-    WLV['SUBENTITY'] = df['SUBENTITY'] 
-    WLV['AREA'] = df['AREA']
-    WLV['BATCH_ID'] = df['BATCH_ID']
-    WLV['OPERATION'] = df['OPERATION']
-    
+    for item in LVL_Columns:
+        WLV[item] = df[item]
+   
     #####################################  Wafer Chamber Association #################################
     
     WLV['SLOT'],WLV['WID'],WLV['WID3'],WLV['CHAMBER'] = WaferChamberAssociation(df)
-    
-    # #########################   New FB Metro Parsing  - need function #####################################################
-    # fb_metro_d = df['FB_METRODATA'].split(",")
-    # fb_metro_idx = df['FB_METRODATA_IDX'].split(";")
-    
-    # slot_metro = {}
-    # for item in fb_metro_idx:
-    #     slot = item.split(',')    
-    #     if slot[0] not in slot_metro:
-    #         slot_metro[slot[0]] = []
-    #     slot_metro[slot[0]].append(fb_metro_d[fb_metro_idx.index(item)])
-
-    # for key in slot_metro:
-    #     try:
-    #         slot_metro[key] = np.mean(np.array(slot_metro[key], dtype = float))
-    #     except ValueError:
-    #         slot_metro[key] = np.nan
-
-    # #For debug
-    # #WLV['Slot Metro Data'] = slot_metro
-    
-    # WLV['PC_METRO_DATA'] = []
-    # for pc_slot in WLV['SLOT']:
-    #     if pc_slot in slot_metro:
-    #         WLV['PC_METRO_DATA'].append(slot_metro[pc_slot])
-    #     else:
-    #         WLV['PC_METRO_DATA'].append("")
         
-    
     ################### FB Metro Parsing #############################################
     WLV['FB_METRODATA'] = FB_Metro_by_Slot(df['FB_METRODATA'],df['FB_METRODATA_IDX'] ,WLV['SLOT'])
     WLV['FB_METRODATA2'] = FB_Metro_by_Slot(df['FB_METRODATA2'],df['FB_METRODATA2_IDX'] ,WLV['SLOT'])
     WLV['FB_METRODATA3'] = FB_Metro_by_Slot(df['FB_METRODATA3'],df['FB_METRODATA3_IDX'] ,WLV['SLOT'])
     ##############################  APC settings conversion by lot ###################
-    try: 
-        WLV['B_PART'], WLV['B_PART_1'], WLV['B_PART_2'], WLV['B_PART_3'] = ColumnDecomposition(df['B_PART'], 'B_PART')
-    except TypeError:
-        print(df['KEY'])
-        print(df['B_PART'])
+#    try: 
+    WLV['B_PART'], WLV['B_PART_1'], WLV['B_PART_2'], WLV['B_PART_3'] = ColumnDecomposition(df['B_PART'], 'B_PART')
+#    except TypeError:
+#        print(df['KEY'])
+#        print(df['B_PART'])
         
         
     WLV['B_TOOL'], WLV['B_TOOL_1'], WLV['B_TOOL_2'], WLV['B_TOOL_3']= ColumnDecomposition(df['B_TOOL'], 'B_TOOL')
     WLV['B_PART_PRIOR'], WLV['B_PART_PRIOR_1'], WLV['B_PART_PRIOR_2'], WLV['B_PART_PRIOR_3'],= ColumnDecomposition(df['B_PART_PRIOR'], 'B_PART_PRIOR')
-    try: 
-        WLV['B_TOOL_RS'], WLV['B_TOOL_RS_1'], WLV['B_TOOL_RS_2'], WLV['B_TOOL_RS_3'] = ColumnDecomposition(df['B_TOOL_RS'], 'B_TOOL_RS')
-    except TypeError:
-        print("problem is ", df['B_TOOL_RS'], df['KEY'])
+    
+#    try: 
+    WLV['B_TOOL_RS'], WLV['B_TOOL_RS_1'], WLV['B_TOOL_RS_2'], WLV['B_TOOL_RS_3'] = ColumnDecomposition(df['B_TOOL_RS'], 'B_TOOL_RS')
+#    except TypeError:
+#        print("problem is ", df['B_TOOL_RS'], df['KEY'])
+        
     WLV['B_PART_RS'], WLV['B_PART_RS_1'], WLV['B_PART_RS_2'], WLV['B_PART_RS_3'] = ColumnDecomposition(df['B_PART_RS'], 'B_PART_RS')
     WLV['SETTING_USED'], WLV['SETTING_USED_1'], WLV['SETTING_USED_2'], WLV['SETTING_USED_3'] = ColumnDecomposition(df['SETTING_USED'], 'SETTING_USED')
-    WLV['TARGET'], WLV['TARGET_1'], WLV['TARGET_2'], WLV['TARGET_3']  = ColumnDecomposition(df['TARGET'], 'TARGET')
-    #Need to add B_TOOL
+    WLV['TARGET'], WLV['TARGET_1'], WLV['TARGET_2'], WLV['TARGET_3']  = ColumnDecomposition(df['TARGET'], 'TARGET')    
     
-    try:
-        wfr1_act, wfr1_act_idx = df['WAFERS1_ACT'].split(','), df['WAFERS1_ACT_IDX'].split(';')
-    except AttributeError:
-        wfr1_act = []              
-        wfr1_act_idx = []
-    try:
-        wfr2_act, wfr2_act_idx = df['WAFERS2_ACT'].split(','), df['WAFERS2_ACT_IDX'].split(';')    
-    except AttributeError:
-        wfr2_act = []              
-        wfr2_act_idx = []
-    try:
-        wfr3_act, wfr3_act_idx = df['WAFERS3_ACT'].split(','), df['WAFERS3_ACT_IDX'].split(';')     
-    except AttributeError:
-        wfr3_act = []              
-        wfr3_act_idx = []
-    except KeyError:
-        wfr3_act = []              
-        wfr3_act_idx = []
+ #   try:
+    wfr1_act, wfr1_act_idx = df['WAFERS1_ACT'].split(','), df['WAFERS1_ACT_IDX'].split(';')
+ #   except AttributeError:
+ #       print("Attribute Error in wfr1act parsing")
+ #       wfr1_act = []              
+ #       wfr1_act_idx = []
+ #   try:
+    wfr2_act, wfr2_act_idx = df['WAFERS2_ACT'].split(','), df['WAFERS2_ACT_IDX'].split(';')    
+#    except AttributeError:
+#       print("Attribute Error in wfr2act parsing")
+#        wfr2_act = []              
+#        wfr2_act_idx = []
+#    try:
+    wfr3_act, wfr3_act_idx = df['WAFERS3_ACT'].split(','), df['WAFERS3_ACT_IDX'].split(';')     
+#    except AttributeError:
+#        print("Attribute Error in wfr3act parsing")
+#        wfr3_act = []              
+#        wfr3_act_idx = []
+#    except KeyError:
+#        print("Key Error in wfr1act parsing")
+#        wfr3_act = []              
+#        wfr3_act_idx = []
         
    
     WLV['WFR1_ACT_BY_SLOT'] = WafersACTValuesBySlot(WLV['SLOT'],  wfr1_act, wfr1_act_idx, df['UPTIME'])
@@ -297,22 +283,35 @@ def WaferLevelData(df):
 
     return WLV
 
-def WaferLevelDataConstruction(wlv_lst):
-    wafer_level_long_df = pd.DataFrame()
-    
+def WaferLevelDataConstruction(wlv_lst):    
+    wafer_level_long_df = pd.DataFrame()    
     for item in wlv_lst:
         temp_df = pd.DataFrame(item)    
         wafer_level_long_df = pd.concat([wafer_level_long_df,temp_df], ignore_index=True)
-    return wafer_level_long_df    
+    return wafer_level_long_df  
+  
 
 def Timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S") 
+
+
+def ParquetPrep(wlv):
+    ColumnsForPQT = ['FB_METRODATA','FB_METRODATA2','FB_METRODATA3']
+        
+    for item in ColumnsForPQT:
+       wlv[item] = wlv[item].fillna('default_value')
+       wlv[item] = wlv[item].astype(str)
+    return wlv
+    
+############################# Output directory definition ############################################
+output_path = "//ORshfs.intel.com/ORanalysis$/1274_MAODATA/GAJT/WIJT/ByPath/GER_fdoktorm/DeconstructionTest/"
+
     
 ###### Real Time Data Extract ##################
 DF = DataExtractFromXEUS()
-#DF.to_csv("//ORshfs.intel.com/ORanalysis$/1274_MAODATA/GAJT/WIJT/ByPath/GER_fdoktorm/DeconstructionTest/RawExtractData.csv", index = False)
+# DF.to_csv(output_path+"RawExtractData.csv", index = False)
 ########################################################################################################################
-#DF = pd.read_csv("//ORshfs.intel.com/ORanalysis$/1274_MAODATA/GAJT/WIJT/ByPath/GER_fdoktorm/DeconstructionTest/RawExtractData.csv")
+# DF = pd.read_csv(output_path+"RawExtractData.csv")
 
 timestamp = Timestamp()   
 print(f"Starting Pivot  {timestamp}")
@@ -327,12 +326,6 @@ timestamp = Timestamp()
 print(f"Quality Check Done {timestamp}")
 
 timestamp = Timestamp() 
-print(f"Saving raw data to SD  {timestamp}")
-DF_Pivot_Checked.to_csv("//ORshfs.intel.com/ORanalysis$/1274_MAODATA/GAJT/WIJT/ByPath/GER_fdoktorm/DeconstructionTest/LotLevelValidationvsUI.csv", index = False)
-timestamp = Timestamp() 
-print(f"Raw data to SD saved  {timestamp}")
-
-timestamp = Timestamp() 
 print(f"Starting Pivot Checking {timestamp}")
 df_wlv = list(DF_Pivot_Checked.apply(WaferLevelData, axis = 1))
 timestamp = Timestamp() 
@@ -344,10 +337,29 @@ WLV_data = WaferLevelDataConstruction(df_wlv)
 timestamp = Timestamp() 
 print(f"WLV data parsing done {timestamp}")
 
+
+# Data Saving in Shared Location
+
+# timestamp = Timestamp() 
+# print(f"Saving raw data to SD  {timestamp}")
+# DF_Pivot_Checked.to_csv(output_path+"LotLevelValidationvsUI.csv", index = False)
+# timestamp = Timestamp() 
+# print(f"Raw data to SD saved  {timestamp}")
+
+
+# timestamp = Timestamp() 
+# print(f"Saving WLV data to SD {timestamp}")
+# WLV_data.to_csv(output_path+"FinalWaferLevelData.csv", index = False)
+# timestamp = Timestamp() 
+# print(f"Saving WLV data to SD done {timestamp}")
+
+
+WLV_data = ParquetPrep(WLV_data)
 timestamp = Timestamp() 
-print(f"Saving WLV data to SD {timestamp}")
-WLV_data.to_csv("//ORshfs.intel.com/ORanalysis$/1274_MAODATA/GAJT/WIJT/ByPath/GER_fdoktorm/DeconstructionTest/FinalWaferLevelData.csv", index = False)
+print(f"Saving WLV data to SD with Parquet {timestamp}")
+WLV_data.to_parquet(output_path+"FinalWaferLevelData.parquet", index = False)
 timestamp = Timestamp() 
-print(f"Saving WLV data to SD done {timestamp}")
+print(f"Saving WLV data to SD with Parquet done {timestamp}")
+
 
 
